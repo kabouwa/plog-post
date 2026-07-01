65eff77e3ed3a8913422be38a026d04944eb2396
Kabouwa <med.rh.med27@gmail.com>
2026-07-01 15:12:20 +0100

    fix switch button action

--- public/js/ui-switcher.js
+++ public/js/ui-switcher.js
@@ -12,4 +12,28 @@
             ui_used = "cards";
         }
     })
+
+    let used_ui = localStorage.getItem('used_ui') ?? "cards" ;
+    used_ui === "table"
+        ? $("#posts-table").removeClass("d-none")
+        : $("#posts-cards").removeClass("d-none")
+
+    const switchUi = () => {
+        let delay = 600;
+        if(used_ui === "cards"){
+            if ( $("#posts-table").hasClass("d-none") ) $("#posts-table").removeClass("d-none");
+            $("#posts-table").slideDown(delay);
+            $("#posts-cards").slideUp(delay)
+            used_ui = "table";
+            localStorage.setItem("used_ui",used_ui)
+        }else if(used_ui === "table"){
+            if ( $("#posts-cards").hasClass("d-none") ) $("#posts-cards").removeClass("d-none");
+            $("#posts-table").slideUp(delay);
+            $("#posts-cards").slideDown(delay)
+            used_ui = "cards";
+            localStorage.setItem("used_ui",used_ui)
+        }
+    }
+    $("#ui-switcher").click(switchUi)
+
 })()
---
+++ resources/js/app.jsx
@@ -0,0 +1,16 @@
+"use client"
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+function textComp(){
+    return(
+        <h1>
+            This is a component
+            hhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </h1>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('app'))
